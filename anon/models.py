6c960711898bd3480d0db3cc0bc96bc114e3f348
Amogh Ganjikunta <amoghganjikunta@gmail.com>
2022-01-23 11:10:07 -0500

    hi

--- anon/models.py
+++ anon/models.py
@@ -16,3 +16,12 @@ class comment(models.Model):
     postid = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=140)
+
+
+class User(AbstractUser):
+    school = models.ForeignKey(
+        "school", on_delete=models.CASCADE, default=True)
+
+
+class school(models.Model):
+    name = models.CharField(max_length=140)
--- anonymousapp/settings.py
+++ anonymousapp/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+AUTH_USER_MODEL = 'anon.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
